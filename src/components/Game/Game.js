16358886b157eed78ca4ccdbc75100e67304a85d
juan carlos guerrero murillo <perromendigo321@gmail.com>
2022-04-04 13:04:03 -0500

    ya

---
+++ src/components/Game/Game.js
@@ -0,0 +1,163 @@
+import React, { Component } from 'react';
+import './Game.css';
+import papel from '../shared/images/papel.png';
+import piedra from '../shared/images/piedra.png';
+import tijeras from '../shared/images/tijeras.png';
+
+class Game extends Component {
+
+  constructor() {
+      super();
+      this.state = {
+          random: 0,
+          pj: 0,
+          pc: 0,
+          eleccion: 0,
+          mensaje: ""
+      }
+  }  
+
+    handleOnPiedra = () =>{
+        let Random = parseInt(this.state.random);
+        let eleccion = 1;
+        Random = generateRandomNumber(3);
+        let text = textoPiedra(Random);
+        let pJ = parseInt(this.state.pj);
+        let pC = parseInt(this.state.pc);
+        if(Random === 1){
+            pC++;
+        }
+        else if(Random === 2){
+            pJ++;
+        }
+        this.setState({mensaje: text,
+        eleccion: 1,
+        random: Random + 1,
+        pj: pJ,
+        pc: pC,
+        }) 
+        
+    }
+
+    handleOnPapel = () =>{
+        let Random = parseInt(this.state.random);
+        Random = generateRandomNumber(3);
+        let text = textoPapel(Random);
+        let pJ = parseInt(this.state.pj);
+        let pC = parseInt(this.state.pc);
+        if(Random === 2){
+            pC++;
+        }
+        else if(Random === 0){
+            pJ++;
+        }
+        this.setState({mensaje: text,
+        eleccion: 2,
+        random: Random +1, 
+        pj: pJ,
+        pc: pC,
+        }) 
+    }
+    
+    handleOnTijeras = () =>{
+        let Random = parseInt(this.state.random);
+        Random = generateRandomNumber(3);
+        let text = textoTijeras(Random);
+        let pJ = parseInt(this.state.pj);
+        let pC = parseInt(this.state.pc);
+        if(Random === 0){
+            pC++;
+        }
+        else if(Random === 1){
+            pJ++;
+        }
+        this.setState({mensaje: text,
+        eleccion: 3,
+        random: Random + 1,
+        }) 
+    }
+
+  render() {
+    return (
+      <div>
+          <header>
+             <h1>Piedra, Papel o Tijeras</h1>
+             <img src={piedra} onClick={this.handleOnPiedra}/>
+            <img src={papel} onClick={this.handleOnPapel}/>  
+            <img src={tijeras} onClick={this.handleOnTijeras}/>
+          </header>
+          <div className="juagador"></div>
+          <h1>Eleccion del juegador</h1>
+         <img src={mostarEleccion(this.state.eleccion)}></img>
+          <div className="cpu">
+          <h1>Eleccion del contricante</h1>
+         <img src={mostarEleccion(this.state.random )}></img>
+          </div>
+          <div className="cartel">
+              <h2>{this.state.mensaje}</h2>
+              <h2>Llevas {this.state.pj} puntos</h2>
+              <h2>El contricante lleva {this.state.pc} puntos</h2>
+          </div>
+
+      </div>
+    )
+  }
+}
+
+export default Game;
+
+function sumarContador(eleccion, random, p1, p2){
+if (eleccion === random + 1){}
+else if (eleccion === 1 && random === 1){p2++; return p2}
+else if (eleccion === 2 && random ===2){p2++; return p2}
+else if (eleccion === 3 && random ===0){p2++; return p2}
+else{p1++; return p1} 
+}
+
+function mostarEleccion(eleccion){
+    if(eleccion === 1){ return piedra}
+    else if(eleccion === 2){return papel}
+    else if(eleccion === 3){return tijeras}
+}
+
+function generateRandomNumber(max) {
+    return Math.floor(Math.random()*(max));
+}
+
+function textoPiedra(random){
+    if(random === 0){
+        return "El rival eligio piedra. Empataste la ronda";
+    }
+    else if(random === 1){
+        return "El rival eligio papel. Perdiste la ronda";
+    }
+    else{
+        return "El rival eligio tijeras. Ganaste la ronda";
+    }
+}
+function textoPapel(random){
+    if(random === 0){
+        return "El rival eligio piedra. Ganaste la ronda";
+    }
+    else if(random === 1){
+        return "El rival eligio papel. Empataste la ronda";
+    }
+    else{
+        return "El rival eligio tijeras. Perdiste la ronda";
+    }
+    
+}
+
+function textoTijeras(random){
+    if(random === 0){
+        return "El rival eligio piedra. Pedriste la ronda";
+    }
+    else if(random === 1){
+        return "El rival eligio papel. Ganaste la ronda";
+    }
+    else{
+        return "El rival eligio tijeras. Empataste la ronda";
+    }
+}
+
+
